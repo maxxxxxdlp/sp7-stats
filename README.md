952e84425a856bfba7efde88ec35a2c929497632
maxxxxxdlp <40512816+maxxxxxdlp@users.noreply.github.com>
2020-06-09 19:32:03 +0300

    Updates documentation

--- README.md
+++ README.md
@@ -4,24 +4,23 @@ This website provides some information on the usage of [Specify 7](https://githu
 ## Requirements
 
 1. PHP 7.4 (older versions may work)
-1. Any Web server
+1. Any Webserver
 
 ## Installation
 
 All settings parameters are located in `config.php`
 
 1. Open the `config.php` file.
-1. Set `LINK` to an address where the server would be located. This is needed to route css and js files properly.
-1. Set `FILES_LOCATION` location to the place where all of your access.log files are located. Make sure the web server has read permissions to all the files in this folder.
-1. Set `UNZIP_LOCATION` to any empty folder. This would be the destination for all uncompressed access.log. Make sure the web server has write permissions to this folder. **Warning!** All of the files present in `UNZIP_LOCATION` would be deleted.
-1. Configure your web server to point to this directory.
+1. Set `LINK` to an address the website would be served on.
+1. Set `FILES_LOCATION` location to the place where all of your access.log files are located. Make sure the webserver has read permissions to all the files in this folder.
+1. Set `WORKING_LOCATION` to an empty folder. This would be the destination for all uncompressed access.log files and other files created in the process. Make sure the webserver has to write permissions to this folder. **Warning!** All of the files present in this directory would be deleted.
+1. Configure your webserver to point to this directory.
 
 ### Optional settings
  
-1. You can setup daily cron to the following location `http://<youraddress>/cron.php`. This will automatically unzip the files so that users can get up to date information.
+1. You can set up daily cron to the following location `http://<yourdomain>/cron.php`. This will automatically unzip the files and compile the information so that users can get up to date information.
 1. You can change the duration of time before compiled data is considered out of date by changing `SHOW_DATA_OUT_OF_DATE_WARNING_AFTER`. The default value is 86400 = 1 day.
-1. You can set which HTTP codes get ignored in the result by changing `HTTP_CODES_TO_EXCLUDE`
-1. By default `SPLIT_DATA` is set to 1000, meaning compiled data would be split into files of 1000 lines
+1. By default `SPLIT_DATA` is set to 1000, meaning compiled data would be split into files of 1000 lines. Increasing this value negatively affects page load performance. Adjust this value based on how much traffic you are getting.
 
 #### IP Filtering
 
@@ -30,3 +29,11 @@ All settings parameters are located in `config.php`
 - `WHITELIST_IP_LOCATION` should point to the file that has one allowed IP per line.
 - All IPs that were blocked, would be written in the `BLOCKED_IPS_LOG_LOCATION` file.
 - All IPs that were allowed, would be logged in the `IPS_LOG_LOCATION` file.
+
+## Credit for used resources
+There were snippets of code/files from the following resources used:
+- [Bootstrap 4.5.0](https://github.com/twbs/bootstrap)
+- [jQuery 3.5.1](https://github.com/jquery/jquery)
+- [Chart.js](https://github.com/chartjs/Chart.js)
+- [user_agent_strings.php](https://gist.github.com/maxxxxxdlp/f5977416b66000746f4abdf861caf1e3)
+- [Specify 7 icon](https://sp7demofish.specifycloud.org/static/img/fav_icon.png)
--- config.php
+++ config.php
@@ -1,55 +1,94 @@
 <?php
 
-/*
- *
- * CONSTANTS (should be defined before including this file):
- * CSS - links a specified CSS file from the 'css' folder (extension not required)
- * JQUERY - whether to include jQuery (bool)(default = FALSE)
- * BOOTSTRAP - whether to include Bootstrap (bool)(default = TRUE)
- * TIMEZONE - specifies the timezone to use (default = 'UTC)
- * NO_HEAD - stops outputting the <head> tag (bool)(default = FALSE)
- * MENU_JS - whether to include `menu.js`. FALSE by default. Is set to TRUE inside of `menu.php`
- *
- * */
 
+# Address the website would be served on
 define('LINK', 'https://sp7.maxxxxxdlp.ml/');
 
 
+
+# Location to the place where all of your access.log files are located.
+# Make sure the webserver has read permissions to all the files in this folder.
 define('FILES_LOCATION','/home/ec2-user/data/Sp7-stats/');
-define('UNZIP_LOCATION','/home/ec2-user/data/Sp7-stats/unzipped/');
-define('USERSTACK_API_KEY_LOCATION','/home/ec2-user/data/Sp7-stats/userstack.com_api.key');//SET TO FALSE TO IGNORE THIS API
 
-# this api is not yet used
-#define('IPSTACK_API_KEY_LOCATION','/home/ec2-user/data/Sp7-stats/ipstack.com_api.key');//SET TO FALSE TO IGNORE THIS API
+# Set this to an empty folder. This would be the destination for all uncompressed
+# access.log and other files created in the process.
+# Make sure the webserver has write permissions to this folder.
+# **Warning!** All of the files present in this directory would be deleted.
+define('WORKING_LOCATION','/home/ec2-user/data/Sp7-stats/files/');
 
 
+
+# Whether to log user IPs
 define('LOG_IPS',TRUE);
+
+# Whether to block external ips
 define('BLOCK_EXTERNAL_IPS',FALSE);
+
+# Location of a text file that contains a list of IPs that are whitelisted
+# IPs should be separated by a new line
 define('WHITELIST_IP_LOCATION','/home/ec2-user/data/whitelist_ip_list.txt');
+
+# Location of a text file that would contain a non-distinct list of IPs that were denied access to your site
+# IPs would be separated by a new line
 define('BLOCKED_IPS_LOG_LOCATION','/home/ec2-user/data/blocked_ip_list.txt');
+
+# Location of a text file that would contain a non-distinct list of IPs that were allowed access to your site
+# IPs would be separated by a new line
 define('IPS_LOG_LOCATION','/home/ec2-user/data/ip_list.txt');
 
 
-define('SHOW_DATA_OUT_OF_DATE_WARNING_AFTER','86400');
+
+# If data was not refreshed for this much time, the user would get a reminder to refresh data
+# Also, if no new records were coming after this much time, an error message would be shown
+define('SHOW_DATA_OUT_OF_DATE_WARNING_AFTER',86400);//one day
+
+# How many records to store in one file before splitting
+# Bigger values decrease performance
+# Adjust it based on how much traffic you are getting
 define('SPLIT_DATA',1000);
 
-define('DELETE_LOG_FILES_IN_UNZIP_LOCATION',TRUE);//deletes .log files in `UNZIP_LOCATION` after they are no longer needed. This can save some space if your machine is low on memory
+
+
+# FOR DEBUG ONLY
+# This will show success actions for most actions performed while the data refresh is running
 define('VERBOSE',FALSE);
 
-define('DEFAULT_BROWSER','');//value to be used if failed to extract the browser from the User Agent String
-define('DEFAULT_OS','');     //value to be used if failed to extract the os      from the User Agent String
 
-//https://www.php.net/manual/en/function.date.php
+
+# Specifies the value to be used if failed to extract the browser from the User-Agent String
+define('DEFAULT_BROWSER','');
+
+# Specifies the value to be used if failed to extract the os      from the User-Agent String
+define('DEFAULT_OS','');
+
+
+
+# Formatting for years, months and days when showing the usage statistics for a particular institution
+# https://www.php.net/manual/en/function.date.php
 define('YEAR_FORMATTER','Y');
 define('MONTH_FORMATTER','F');
-define('DAY_FORMATTER','J D');
+define('DAY_FORMATTER','j D');
 
-const HTTP_CODES_TO_ACCEPT = [
-	'204',//empty response
-];
 
-//const LINKS_TO_CAPTURE = [
-//	'/capture?',
-//];
 
-define('COLUMNS',['IP Address','Date','SP7','SP6','Institution','Discipline','Collection','Isa Number','Browser','OS']);
+# Specifies how to display the results on the main page
+# These values can be overwritten by supplying $_GET['view'] (e.x. ?view=00)
+# 0  - Shows list view by default. Table view is also available
+# 1  - Shows table view by default. List view is also available
+# 00 - Shows list only. Table view is not available
+# 11 - Shows table view only. List view is not available
+define('MAIN_PAGE_OUTPUT_FORMAT','0');
+
+
+
+# FOR DEVELOPMENT
+
+# You can define the following constants before including the header.php or menu.php files
+# This would allow you to customize various options
+#
+# CSS       - links a specified CSS file from the 'css' folder (extension not required) (e.x. main)
+# JS        - links a specified JS  file from the 'js'  folder (extension not required) (e.x. institutions)
+# BOOTSTRAP - whether to include Bootstrap (bool)(default = TRUE )
+# JQUERY    - whether to include jQuery    (bool)(default = FALSE)
+# NO_HEAD   - does not output the <html>, <head> tags and their content (bool)(default = FALSE)
+# MENU_JS   - whether to include `menu.js`. FALSE by default. Is set to TRUE inside of `menu.php`
