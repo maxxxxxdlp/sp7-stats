9e6d583011bb04ce8ee7f054eb3d932fa0f5d72a
Max Patiiuk <max@patii.uk>
2020-06-30 22:01:15 +0300

    Revert "Adds domain as a root ordering level"
    This reverts commit 87975afda702bad2ee5dc593b5bff747e1d98a13.

--- config/required.php
+++ config/required.php
@@ -1,59 +1,38 @@
 <?php
 
+//define('DEVELOPMENT',TRUE);
 
-if($_SERVER['HTTP_HOST']=='localhost'){
-	define('DEVELOPMENT',TRUE);
-	define('CONFIGURATION','localhost');
-}
-elseif($_SERVER['HTTP_HOST']=='sp7.maxxxxxdlp.ml'){
-	define('DEVELOPMENT',TRUE);
-	define('CONFIGURATION','ec2');
-}
-else {
+# You can have different constant's values for your development and production servers
+# You can modify the code to read the value for `DEVELOPMENT` constant from some other file
+if($_SERVER['HTTP_HOST']=='biwebdbtest.nhm.ku.edu')
 	define('DEVELOPMENT',FALSE);
-	define('CONFIGURATION','production');
-}
+else
+	define('DEVELOPMENT',TRUE);
 
-
-if(CONFIGURATION==='localhost'){
+if(DEVELOPMENT){ # these settings would be used during development
 
 	# Address the website would be served on
-	define('LINK', 'http://localhost:80/');
+	define('LINK', 'https://sp7.maxxxxxdlp.ml/');
 
 	# Location to the place where all of your access.log files are located.
 	# Make sure the web server has read permissions to all the files in this folder.
-	define('FILES_LOCATION','/Users/mambo/Downloads/Sp7-stats/');
+	define('FILES_LOCATION','/home/ec2-user/data/Sp7-stats/');
 
 	# Set this to an empty folder. This would be the destination for all uncompressed
 	# access.log and other files created in the process.
 	# Make sure the web server has write permissions to this folder.
 	# **Warning!** All of the files present in this directory would be deleted.
-	define('WORKING_LOCATION','/Users/mambo/Downloads/Sp7-stats/files/');
-
-	# File that stores $github_username and $github_token for GitHub API to work
-	define('GITHUB_TOKEN_LOCATION','/Users/mambo/Downloads/Sp7-stats/github_tokens.php');
-
-}
-
-elseif(CONFIGURATION==='ec2'){
-
-	define('LINK', 'https://sp7.maxxxxxdlp.ml/');
-
-	define('FILES_LOCATION','/home/ec2-user/data/Sp7-stats/');
-
 	define('WORKING_LOCATION','/home/ec2-user/data/Sp7-stats/files/');
 
+	# File that stores $github_username and $github_token for GitHub API to work
 	define('GITHUB_TOKEN_LOCATION','/home/ec2-user/data/github_tokens.php');
 
 }
 else { # these settings would be used during production
 
 	define('LINK', 'http://biwebdbtest.nhm.ku.edu/sp7-stats/');
-
 	define('FILES_LOCATION','/home/anhalt/sp7-stats-logs/');
-
 	define('WORKING_LOCATION','/home/sp7-stats/tmp/');
-
 	define('GITHUB_TOKEN_LOCATION','/home/sp7-stats/tmp/github_tokens.php');
 
 }
--- static/js/stats.js
+++ static/js/stats.js
@@ -1,15 +1,13 @@
-function update_stats(domains_count=null,
-                      institutions_count=null,
+function update_stats(institutions_count=null,
                       disciplines_count = null,
                       collections_count = null,
                       reports_count = null){
 
-	if(domains_count==null)
-		[domains_count,institutions_count,disciplines_count,collections_count,reports_count] = fetch_stats();
+	if(institutions_count==null)
+		[institutions_count,disciplines_count,collections_count,reports_count] = fetch_stats();
 
 	const stats = $('#stats');
-	stats.html(domains_count+` domains<br>` +
-		institutions_count+` institutions<br>` +
+	stats.html(institutions_count+` institutions<br>` +
 		disciplines_count+` disciplines<br>` +
 		collections_count+` collections<br>` +
 		reports_count+` reports`);
@@ -18,29 +16,26 @@ function update_stats(domains_count=null,
 
 function fetch_stats(){
 
-	let domains_count;
-	let institutions_count;
-	let disciplines_count;
-	let collections_count;
+	let institutions_count = 0;
+	let disciplines_count = 0;
+	let collections_count = 0;
 	let reports_count = 0;
 
 	if(search_mode === 'table'){
 
-		const domains = $('tbody:not([style="display: none;"])');
-		domains_count = domains.length;
-
-		const institutions = domains.find('.institution');
+		const institutions = $('tbody:not([style="display: none;"])');
 		institutions_count = institutions.length;
 
-		const disciplines = domains.find('.discipline');
+		const disciplines = institutions.find('.discipline');
 		disciplines_count = disciplines.length;
 
-		const collections = domains.find('[data-reports_count]');
+		const collections = institutions.find('[data-reports_count]');
 		collections_count = collections.length;
 
 		$.each(collections,function(key,element){
 
 			const el = $(element);
+
 			reports_count += parseInt(el.attr('data-reports_count'));
 
 		});
@@ -48,10 +43,7 @@ function fetch_stats(){
 	}
 	else {
 
-		const domains = $('ol:not(.breadcrumb) > li:not([style="display: none;"])');
-		domains_count = domains.length;
-
-		const institutions = domains.find('> ul > li');
+		const institutions = $('ol:not(.breadcrumb) > li:not([style="display: none;"])');
 		institutions_count = institutions.length;
 
 		const disciplines = institutions.find('> ul > li');
@@ -63,18 +55,19 @@ function fetch_stats(){
 		$.each(collections,function(key,element){
 
 			const el = $(element);
+
 			reports_count += parseInt(el.attr('data-reports_count'));
 
 		});
 
 	}
 
-	return [domains_count,institutions_count,disciplines_count,collections_count,reports_count];
+	return [institutions_count,disciplines_count,collections_count,reports_count];
 
 }
 
 $(function(){
 
-	update_stats(domain_count, institution_count,discipline_count,collection_count,report_count);
+	update_stats(institution_count,discipline_count,collection_count,report_count);
 
 });
