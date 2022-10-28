96853a48e224c213a10c2d6878c4da94d4dd22ef
Max Patiiuk <max@patii.uk>
2022-10-28 11:53:27 -0500

    Move source code into a separate directory

--- sp7-stats/components/header.php
+++ sp7-stats/components/header.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once(dirname(__FILE__).'/functions.php');
+require_once(dirname(__FILE__) . '/functions.php');
 require_file('../config/required.php');
 require_file('../config/optional.php');
 
